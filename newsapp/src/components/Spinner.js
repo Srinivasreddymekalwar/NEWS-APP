cf9843d0e413411985705d57bd23eb957191b505
srinivasreddymekalwar <srinivasreddymekalwar@gmail.com>
2024-10-22 21:36:13 +0530

    first commit

---
+++ newsapp/src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div>
+        <img src="C:\Users\user\Downloads\spinner.gif.gif" alt="loading"/>
+      </div>
+    )
+  }
+}
+
+export default Spinner
